993eda769eba89f6bfabf3b5b04fc601aa77937c
michaelolof <myqrrlolof@yahoo.com>
2018-05-19 07:16:35 +0100

    Developed a working prototype @ Sat, May 19, 2018  7:16:34 AM

--- dist/bin/index.js
+++ dist/bin/index.js
@@ -10,7 +10,55 @@ var utilities_1 = require("../utilities");
  * 1). node.js tsmix-linter/bin "somefile.ts"
  * 2). node.js tsmix-linter/bin --root "c:/Users/some/path/to/the/root"
  */
-parseArguments(process.argv);
+parseArguments(process.argv.slice(2));
+var lintMethod;
+var options = {
+    "--root": undefined,
+    "--log": "minimal",
+};
+function parseArguments(args) {
+    var rootLoc = args.indexOf("--root");
+    var logLoc = args.indexOf("--log");
+    var filePath = args[0];
+    var rootValue = undefined;
+    var logValue = "minimal";
+    if (rootLoc > -1)
+        rootValue = args[rootLoc + 1];
+    if (logLoc > -1)
+        logValue = validateLog(args[logLoc + 1]);
+    if (rootValue === undefined && filePath) {
+        return lintFile(filePath, logValue);
+    }
+    if (rootValue) {
+        return lintFileByRootPath(rootValue, logValue);
+    }
+    console.log(helperLog());
+    return;
+    // if( rootValue )
+    function validateLog(log) {
+        if (log === "diagnostic" || log === "normal" || log === "minimal")
+            return log;
+        else
+            return "minimal";
+    }
+}
+// function parseArguments(args:string[]) {
+//   const arg2 = args[2];
+//   if( arg2 === undefined ) {
+//     return helperLog();
+//   }
+//   if( arg2 === "--path" ) { 
+//     const rootPath = args[3];
+//     if( rootPath === undefined ) return helperLog("Undefined root path");
+//     else return tryAndLintFileByRootPath( rootPath );
+//   } 
+//   else if( arg2.startsWith("--") || arg2.startsWith("-") ) {
+//     return helperLog();
+//   } 
+//   else {
+//     return lintFile( arg2 );
+//   }
+// }
 function helperLog(additionalInfo) {
     if (additionalInfo === void 0) { additionalInfo = ""; }
     return additionalInfo + "Unkown Format: You can lint a file or a set of files by;\n" +
@@ -19,36 +67,45 @@ function helperLog(additionalInfo) {
         "or\n" +
         "tsmix-linter/bin --path 'Users/JohnDoe/location/to/root/path'";
 }
-function parseArguments(args) {
-    var arg2 = args[2];
-    if (arg2 === undefined) {
-        return helperLog();
-    }
-    if (arg2 === "--path") {
-        var rootPath = args[3];
-        if (rootPath === undefined)
-            return helperLog("Undefined root path");
-        else
-            return tryAndLintFileByRootPath(rootPath);
-    }
-    else if (arg2.startsWith("--") || arg2.startsWith("-")) {
-        return helperLog();
-    }
-    else {
-        return tryAndLintFileName(arg2);
-    }
-}
-function tryAndLintFileName(fileName) {
+function lintFile(fileName, log) {
     fileName = utilities_1.normalizePath(fileName);
     app_1.App
         .launchCompiler(fileName)
-        .then(console.log);
+        .then(function (diagnostics) { return diagnostics.map(function (diagnostic) { return logMatcher(diagnostic, log); }); });
 }
-function tryAndLintFileByRootPath(rootPath) {
+function lintFileByRootPath(rootPath, log) {
     app_1.App.launchWatcher(rootPath, function (program) {
         if (app_1.App.rootFiles) {
-            __1.validateAll(program.getProgram(), app_1.App.rootFiles).then(console.log);
+            __1.validateAll(program.getProgram(), app_1.App.rootFiles)
+                .then(function (diagnostics) { return diagnostics.map(function (diagnostic) { return logMatcher(diagnostic, log); }); });
         }
     });
+}
+function logMatcher(diagnostic, log) {
+    switch (log) {
+        case "diagnostic":
+            diagnosticLogger(diagnostic);
+            break;
+        case "normal":
+            console.log("normal logger");
+            normalLogger(diagnostic);
+            break;
+        case "minimal":
+            minimalLogger(diagnostic);
+            break;
+    }
+}
+function normalLogger(diagnostic) {
+    console.warn("error:(" + diagnostic.range.start.line + "," + diagnostic.range.start.character + ")", diagnostic.message.replace("\n", ""), "on", diagnostic.filePath);
+    console.log("\n");
+}
+function diagnosticLogger(diagnostic) {
+    console.warn(diagnostic);
+    console.log("\n");
+}
+function minimalLogger(diagnostic) {
+    var rel = diagnostic.filePath.replace(process.cwd().replace("\\", "/"), "");
+    console.warn("error: (" + diagnostic.range.start.line + "," + diagnostic.range.start.character + "). " + diagnostic.message.split("\n")[0] + " on " + rel);
+    console.log("\n");
 }
 //# sourceMappingURL=index.js.map
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,6 +1,7 @@
 import { App } from '../app';
 import { validateAll } from '..';
 import { normalizePath } from '../utilities';
+import { Diagnostic } from 'ts-parser';
 /**
  * This program represents the command line interface for the tsmix-linter library
  * ---------------------------------------------------------------------------------------
@@ -9,7 +10,75 @@ import { normalizePath } from '../utilities';
  * 2). node.js tsmix-linter/bin --root "c:/Users/some/path/to/the/root"
  */
 
-parseArguments( process.argv );
+parseArguments( process.argv.slice(2) );
+
+let lintMethod:"by-file"|"by-path"|undefined;
+
+
+type LogParam = "diagnostic" | "minimal" | "normal"
+
+interface ParamtersToValue {
+  "--root":string | undefined
+  "--log": LogParam
+}
+
+
+const options:ParamtersToValue = {
+  "--root": undefined,
+  "--log": "minimal",
+}
+
+function parseArguments(args:string[]) {
+  const rootLoc  = args.indexOf( "--root" );
+  const logLoc = args.indexOf( "--log" );
+  const filePath:string|undefined = args[ 0 ];
+  let rootValue:string | undefined = undefined
+  let logValue:LogParam = "minimal";
+  
+  if( rootLoc > -1 ) rootValue = args[ rootLoc + 1 ]
+
+  if( logLoc > -1 ) logValue = validateLog( args[ logLoc + 1 ] );
+
+  if( rootValue === undefined && filePath ) {
+    return lintFile( filePath, logValue )
+  }
+
+  if( rootValue ) {
+    return lintFileByRootPath( rootValue, logValue )
+  }
+
+  console.log( helperLog() );
+  return 
+  // if( rootValue )
+
+
+  function validateLog(log:string):LogParam {
+    if( log === "diagnostic" || log === "normal" || log === "minimal" ) return log;
+    else return "minimal"
+  }
+
+}
+
+
+// function parseArguments(args:string[]) {
+//   const arg2 = args[2];
+//   if( arg2 === undefined ) {
+//     return helperLog();
+//   }
+  
+//   if( arg2 === "--path" ) { 
+//     const rootPath = args[3];
+//     if( rootPath === undefined ) return helperLog("Undefined root path");
+//     else return tryAndLintFileByRootPath( rootPath );
+//   } 
+//   else if( arg2.startsWith("--") || arg2.startsWith("-") ) {
+//     return helperLog();
+//   } 
+//   else {
+//     return lintFile( arg2 );
+//   }
+
+// }
 
 function helperLog(additionalInfo = "") {
   return additionalInfo + "Unkown Format: You can lint a file or a set of files by;\n" +
@@ -19,38 +88,51 @@ function helperLog(additionalInfo = "") {
   "tsmix-linter/bin --path 'Users/JohnDoe/location/to/root/path'";
 }
 
-function parseArguments(args:string[]) {
-  const arg2 = args[2];
-  if( arg2 === undefined ) {
-    return helperLog();
-  }
-  
-  if( arg2 === "--path" ) { 
-    const rootPath = args[3];
-    if( rootPath === undefined ) return helperLog("Undefined root path");
-    else return tryAndLintFileByRootPath( rootPath );
-  } 
-  else if( arg2.startsWith("--") || arg2.startsWith("-") ) {
-    return helperLog();
-  } 
-  else {
-    return tryAndLintFileName( arg2 );
-  }
 
-}
-
-function tryAndLintFileName(fileName:string) {
+function lintFile(fileName:string, log:LogParam) {
   fileName = normalizePath( fileName )
   App
   .launchCompiler( fileName )
-  .then( console.log )
+  .then( diagnostics => diagnostics.map( diagnostic => logMatcher(  diagnostic, log ) ) );  
 }
 
-function tryAndLintFileByRootPath(rootPath:string) {
+function lintFileByRootPath(rootPath:string, log:LogParam) {
   App.launchWatcher( rootPath, program => {
     if( App.rootFiles ) {
-      validateAll( program.getProgram(), App.rootFiles ).then( console.log );
+      validateAll( program.getProgram(), App.rootFiles )
+      .then( diagnostics => diagnostics.map( diagnostic => logMatcher(  diagnostic, log ) ) );  
     }
   });
+}
+
+function logMatcher( diagnostic:Diagnostic, log:LogParam) {
+  switch( log ) {
+    case "diagnostic":
+      diagnosticLogger( diagnostic );
+      break;
+    case "normal":
+      console.log("normal logger")
+      normalLogger( diagnostic );
+      break
+    case "minimal":
+      minimalLogger( diagnostic );
+      break;
+  }
+}
+
+function normalLogger(diagnostic:Diagnostic) {
+  console.warn( `error:(${diagnostic.range.start.line},${diagnostic.range.start.character})`, diagnostic.message.replace("\n", ""), "on", diagnostic.filePath ) 
+  console.log("\n");  
+}
+
+function diagnosticLogger(diagnostic: Diagnostic) {
+  console.warn( diagnostic );
+  console.log("\n");
+}
+
+function minimalLogger(diagnostic:Diagnostic) {
+  const rel = diagnostic.filePath.replace( process.cwd().replace("\\", "/"), "" );
+  console.warn(`error: (${diagnostic.range.start.line},${diagnostic.range.start.character}). ${diagnostic.message.split("\n")[0]} on ${rel}` );
+  console.log("\n");
 }
 
