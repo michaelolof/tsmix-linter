7ac4181c49100cef6497d90c73311d61c2f55c6c
michaelolof <myqrrlolof@yahoo.com>
2018-05-28 11:22:17 +0100

    Finished coding @ Mon, May 28, 2018 11:22:17 AM

--- bash/update-all.sh
+++ bash/update-all.sh
@@ -1,4 +1,4 @@
-update-npm-version.sh $1 "C:/Users/Michael/Desktop/M.O.O/Programs/custom-npm-modules/tsmix-linter/package.json" 
+update-npm-version.sh $1 "../package.json" 
 git-commit.sh $2
 git push origin master
 npm publish
